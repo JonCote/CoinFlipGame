50e35108a94057921f3472bb2539d6ba127f71b1
JonCote <joncote95@gmail.com>
2019-04-29 13:49:00 -0700

    updated Counter object to encapsulate the data, built object methods to increment count and return count for use.

--- src/game.py
+++ src/game.py
@@ -11,20 +11,47 @@ from tkinter import *
 class Counter:
 
     def __init__(self):
-        self.count = 0
+        """
+        initialize private count variable to 0
+        """
+        self.__count = 0
+
+    def count_up(self):
+        """
+        add one to count
+        :return: None
+        """
+        self.__count += 1
+
+    def count_out(self):
+        """
+        returns count as a int
+        :return: count
+        """
+        return self.__count
 
 
 def coin_flipper():
-    flipCount.count += 1
-    flipCountDisplay.config(text="Flips: "+str(flipCount.count))
+    """
+    increment flip counter
+    update GUI flip counter
+    generate random number from 0 to 1
+    if number is >= 0.5
+    increment score counter
+    update info text to win
+    else
+    update info text to lose
+    :return: None
+    """
+    flipCount.count_up()
+    flipCountDisplay.config(text="Flips: "+str(flipCount.count_out()))
     x = random.uniform(0, 1)
     if x >= 0.5:
-        scoreCount.count += 1
+        scoreCount.count_up()
         text.config(text="You Win")
-        score.config(text="Score: "+str(scoreCount.count))
+        score.config(text="Score: "+str(scoreCount.count_out()))
     else:
         text.config(text="You Lose")
-    print(x)
 
 
 flipCount = Counter()
@@ -42,11 +69,11 @@ text = tk.Message(master=back, width=400, text="Hello Welcome to the Coin Flippe
 text.pack(pady=20)
 
 
-score = tk.Message(master=back, width=100, text="Score: "+str(scoreCount.count))
+score = tk.Message(master=back, width=100, text="Score: "+str(scoreCount.count_out()))
 score.pack(side=BOTTOM, pady=20)
 
 
-flipCountDisplay = tk.Message(master=back, width=100, text="Flips: "+str(flipCount.count))
+flipCountDisplay = tk.Message(master=back, width=100, text="Flips: "+str(flipCount.count_out()))
 flipCountDisplay.pack(side=BOTTOM, pady=20)
 
 
